9fdfb1075ce6a10524ac3db8365d097984e52ac4
Marouane <ettebaa.marouane@gmail.com>
2015-07-31 19:35:56 +0100

    debug

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,14 +2,14 @@ class TagsController < ApplicationController
 
 	def create
 		@article = Article.find(params[:article_id])
-		@comment = @article.tags.create(content: params[:content])
+		@tag = @article.tags.create(content: params[:content])
 		redirect_to article_path(@article)
 	end
 
 	def destroy
 		@article = Article.find(params[:article_id])
-		@comment = @article.tags.find(params[:id])
-		@comment.destroy
+		@tag = @article.tags.find(params[:id])
+		@tag.destroy
 		redirect_to article_path(@article)
 	end
 
